d09503909bd36776e414112cb00027bda86a3b7a
João Alexandre Lucena Ferrão <joaoferrao77@gmail.com>
2023-01-25 23:46:40 -0300

    Implementando padrão Template Method

--- testes.php
+++ testes.php
@@ -3,6 +3,8 @@
 use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadoraDeImpostos;
 use Alura\DesignPattern\Impostos\ICMS;
+use Alura\DesignPattern\Impostos\ICPP;
+use Alura\DesignPattern\Impostos\IKCV;
 use Alura\DesignPattern\Impostos\ISS;
 use Alura\DesignPattern\Orcamento;
 
@@ -14,7 +16,7 @@ $orcamento = new Orcamento();
 $orcamento->valor = 100;
 
 echo "ICMS: " .$calculadora->calcula($orcamento, new ICMS());
-echo "<br>ISS: " .$calculadora->calcula($orcamento, new ISS());*/
+echo "<br>ISS: " .$calculadora->calcula($orcamento, new ISS());
 
 $calculadora = new CalculadoraDeDescontos();
 
@@ -22,4 +24,16 @@ $orcamento = new Orcamento();
 $orcamento->valor = 600;
 $orcamento->quantidadeItens = 5;
 
-echo "Desconto: " . $calculadora->calculaDescontos($orcamento);
+echo "Desconto: " . $calculadora->calculaDescontos($orcamento);*/
+
+$orcamento = new Orcamento();
+$orcamento->valor = 600;
+$orcamento->quantidadeItens = 5;
+
+$imposto = new ICPP();
+
+echo "ICPP: " . $imposto->calculaImposto($orcamento);
+
+$imposto = new IKCV();
+
+echo "<br>ICPP: " . $imposto->calculaImposto($orcamento);
